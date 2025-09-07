7b43c570cd14c2bfa1766f9f074bc82d9ed207c4
Vaishnavi-440 <vaishnavipardeshi440@gmail.com>
2025-09-08 00:12:58 +0530

    Add app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("programming world")
+st.text("WELCOME TO PROFRAMMING EARTH")
+lan=st.selectbox("select the language:",["c++","python","java","html"])
+st.write(f"Your choose {lan}.Excellent choice")
+st.success("your lan has been breded")
