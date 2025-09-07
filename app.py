8d214ca4cb74c7110fb7ac51bd51b8b9f8125b03
Vaishnavi-440 <vaishnavipardeshi440@gmail.com>
2025-09-08 00:38:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ lan=st.selectbox("select the language:",["c++","python","java","html"])
 st.write(f"Your choose {lan}.Excellent choice")
 if st.button("submit"):
   st.success("your had successfully choosen the language.SUBMISSION DONE!")
-add_lan=st.checkbox(["javascript","css","c"])
+add_lan=st.input("enter your fav language:")
 if add_lan:
   st.success(f"your {add_lan} is added succesfully")
 st.text("THANK YOU")
