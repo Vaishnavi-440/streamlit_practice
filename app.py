87d4c867b53d6fc2f10fb44bec5b5fb9e7c03dca
Vaishnavi-440 <vaishnavipardeshi440@gmail.com>
2025-09-08 00:33:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ st.title("programming world")
 st.text("WELCOME TO PROFRAMMING EARTH")
 lan=st.selectbox("select the language:",["c++","python","java","html"])
 st.write(f"Your choose {lan}.Excellent choice")
+if st.button("submit"):
+  st.success("your had successfully choosen the language.SUBMISSION DONE!")
 st.success("your lan has been breded")
 st.text("THANK YOU")
