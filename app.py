03f6f82229567455cfb77651a1debc50d9fc901f
Vaishnavi-440 <vaishnavipardeshi440@gmail.com>
2025-09-08 11:43:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Programming World")
-st.markdown(**WELCOME TO PROFRAMMING EARTH**)
+st.markdown("_Italic word_")
 lan=st.selectbox("select the language:",["c++","python","java","html"])
 st.write(f"Your choose {lan}.Excellent choice")
 if st.button("submit"):
